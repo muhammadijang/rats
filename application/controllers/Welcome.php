adec90b7dc70bb4aff0253d395574c810326e6e7
Muhammad Ijang <muhammadrizal@if.undip.ac.id>
2018-10-26 16:50:24 +0700

    Template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,8 +18,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library(array('template'));
+	}
+	
 	public function index()
 	{
-		$this->load->view('v_dashboard');
+		$data = array('title' => 'Dashboard', );
+		$this->template->admin('content/dashboard',$data);
+		// $this->load->view('v_dashboard');
 	}
 }
---
+++ application/libraries/Template.php
@@ -0,0 +1,20 @@
+<?php
+
+class Template
+{
+    protected $_CI;
+    public function __construct()
+    {
+        $this->_CI=&get_instance();
+    }
+    public function admin($admin, $data=null)
+    {
+        $data['header'] = $this->_CI->load->view('template/header',$data,true);
+        $data['content'] = $this->_CI->load->view($admin,$data,true);
+        $data['sidebar'] = $this->_CI->load->view('template/sidebar',$data,true);
+        $this->_CI->load->view('template/template_admin.php',$data);
+    }
+}
+
+
+?>
